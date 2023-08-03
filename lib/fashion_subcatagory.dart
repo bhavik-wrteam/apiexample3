9b118f91c7b04e6fe9cabdddbc41114ac911c78e
bhavikbhuva <bhavik.wrteam@icloud.com>
2023-08-03 16:57:00 +0530

    snackbar added

--- lib/fashion_subcatagory.dart
+++ lib/fashion_subcatagory.dart
@@ -1,18 +1,61 @@
+import 'dart:convert';
+
 import 'package:apiexample3/product_model.dart';
 import 'package:flutter/material.dart';
 import 'methods.dart';
+import 'package:http/http.dart';
 
 class FashionSubCategory extends StatelessWidget {
   int index;
-  List<Datum> list = [];
+  List<Datum> list;
+
   FashionSubCategory(this.index, this.list, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    print(list[index].children.length);
     return SafeArea(
       child: Scaffold(
-        body: buildFutureList(),
+        body: ListView.builder(
+            itemCount: list[index].children.length,
+            itemBuilder: (context, listViewIndex) {
+              Size size = MediaQuery.of(context).size;
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(
+                      list[index].children[listViewIndex].image,
+                      height: size.height * 0.4,
+                      width: size.width * 0.4,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "name : ${list[index].children[listViewIndex].name}",
+                          style: textStyle(),
+                        ),
+                        Text(
+                          "ID : ${list[index].children[listViewIndex].id}",
+                          style: textStyle(),
+                        ),
+                        Text(
+                          "Counts : ${list[index].children[listViewIndex].clicks}"
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              );
+            }),
       ),
     );
   }
+
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,10 @@
 
+import 'dart:convert';
+
+import 'package:apiexample3/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'fashion_subcatagory.dart';
 import 'methods.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  List<Datum> productList=[];
 
 
 
@@ -26,6 +32,141 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  FutureBuilder<List<Datum>> buildFutureList() {
+
+
+
+    return FutureBuilder(
+      future: getProductData(),
+      builder: (context, snapshot) {
+        final Size size = MediaQuery.of(context).size;
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          print('snapshot = $snapshot');
+          return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.red,
+              ));
+        }
+
+        if (snapshot.hasData) {
+          print('snapshot = $snapshot');
+          return ListView.builder(
+            itemCount: productList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    if(productList[index].children.isNotEmpty){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (builder) => FashionSubCategory(index,productList)),
+                      );
+                    }
+                    else{
+                       const SnackBar snackBar = SnackBar(
+                        content: Text("No Sub Categories exist"),
+                        duration: Duration(seconds: 3),
+                      );
+                       ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    height: MediaQuery.of(context).size.height / 4,
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network(
+                            productList[index].image,
+                            height: size.height * 0.4,
+                            width: size.width * 0.4,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "name : ${productList[index].name}",
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "id : ${productList[index].id}",
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "clicks : ${productList[index].clicks}",
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        } else {
+          return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+              ));
+        }
+      },
+    );
+  }
+
+
+  Future<List<Datum>> getProductData() async {
+    final response = await post(
+        Uri.parse('https://eshopweb.store/app/v1/api/get_categories'),
+        headers: headers);
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      print("DAta fetched successfully");
+      print(data);
+
+      String message = data['message'];
+      print(message);
+      productList =
+          (data['data'] as List).map((e) => Datum.fromJson(e)).toList();
+
+      print("products returned successfully");
+      return productList;
+    } else {
+      print("returned empty list");
+      return productList;
+    }
+  }
+
+  Map<String, String> get headers => {
+    "Authorization":
+    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2OTEwMzY4NDEsImlzcyI6ImVzaG9wIiwiZXhwIjoxNjkxMjUyODQxfQ.Bs_tZSIMRjQgwlin9J8eseR-bmT1pD15LyeKQHS2mGk',
+  };
+
 
 
 
